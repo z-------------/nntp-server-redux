12f05664ee1af1f086f6d42a08830d9c41edf403
Zack Guard <zacharyguard@gmail.com>
2019-10-01 14:48:41 +0800

    Port wx endpoint

--- functions/wx.js
+++ functions/wx.js
@@ -1,6 +1,39 @@
-exports.handler = (e, ctx, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, Netlify Functions!"
-  });
+const p = require("phin");
+
+exports.handler = (e, ctx, callback) => {  
+  let ip = e.queryStringParameters.ip || e.headers["client-ip"]; 
+  let lang = e.queryStringParameters.lang || "en";
+
+  let latLngStr;
+  if (e.queryStringParameters.coords) {
+    latLngStr = e.queryStringParameters.coords;
+    gotLatLngStr();
+  } else {
+    p({
+      url: `https://ipinfo.io/${ip}/geo`,
+      headers: {
+        "Authorization": `Bearer ${process.env.IPINFO_APIKEY}`
+      },
+      parse: "json"
+    }).then(r => {
+      latLngStr = r.body.loc;
+      gotLatLngStr();
+    });
+  }
+
+  function gotLatLngStr() {
+    p({
+      url: `https://api.darksky.net/forecast/${process.env.DARKSKY_APIKEY}/${latLngStr}?units=si&exclude=daily,hourly&lang=${lang}`,
+      parse: "json"
+    }).then(r => {
+      let response = {
+        weather: r.body,
+        debug: {
+          ip,
+          coords: latLngStr.split(",")
+        }
+      };
+      callback(null, { body: JSON.stringify(response) });
+    });
+  }
 };
