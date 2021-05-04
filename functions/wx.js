b1784d00d96651b2cd7abc09ba75250e29e0a402
Zack Guard <zacharyguard@gmail.com>
2021-05-05 02:36:44 +0800

    Add some console logs

--- functions/wx.js
+++ functions/wx.js
@@ -4,6 +4,8 @@ exports.handler = (e, ctx, callback) => {
   let ip = e.queryStringParameters.ip || e.headers["client-ip"]; 
   let lang = e.queryStringParameters.lang || "en";
 
+  console.log("ip:", ip);
+
   let latLngStr;
   if (e.queryStringParameters.coords) {
     latLngStr = e.queryStringParameters.coords;
@@ -17,6 +19,9 @@ exports.handler = (e, ctx, callback) => {
       parse: "json"
     }).then(r => {
       latLngStr = r.body.loc;
+      console.log("body:", r.body);
+      console.log("body.loc:", r.body.loc);
+      console.log("latLngStr:", latLngStr);
       gotLatLngStr();
     });
   }
@@ -26,6 +31,8 @@ exports.handler = (e, ctx, callback) => {
       url: `https://api.darksky.net/forecast/${process.env.DARKSKY_APIKEY}/${latLngStr}?units=si&exclude=daily,hourly&lang=${lang}`,
       parse: "json"
     }).then(r => {
+      console.log("latLngStr:", latLngStr);
+      console.log("latLngStr.split(','):", latLngStr.split(","));
       let response = {
         weather: r.body,
         debug: {
