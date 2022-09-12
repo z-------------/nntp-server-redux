29c164aa4f8b504e80610fedbc7b645d557afe9d
Zack Guard <zacharyguard@gmail.com>
2022-09-12 15:50:03 +0800

    Fix and asyncify

--- functions/wx.js
+++ functions/wx.js
@@ -1,56 +1,57 @@
 const p = require("phin");
-console.log("p is", p);
 
-exports.handler = (e, ctx, callback) => {
-  console.log({ e });
-  console.log("e.queryStringParameters:", e.queryStringParameters);
-  console.log("e.headers:", e.headers);
-
+exports.handler = async (e, _) => {
   const ip = e.queryStringParameters.ip || e.headers["x-nf-client-connection-ip"]; 
   const lang = e.queryStringParameters.lang || "en";
 
-  console.log({ ip, lang });
-
-  let latLngStr;
-  if (e.queryStringParameters.coords) {
-    latLngStr = e.queryStringParameters.coords;
-    gotLatLngStr();
-  } else {
-    p({
-      url: `https://ipinfo.io/${ip}/geo`,
-      headers: {
-        "Authorization": `Bearer ${process.env.IPINFO_APIKEY}`
-      },
-      parse: "json"
-    }).then(r => {
-      latLngStr = r.body.loc;
-      gotLatLngStr();
-    }).catch(err => {
-      console.error("Call to ipinfo.io API failed:", err);
-      callback(null, { status: 500 });
-    });
+  const latLngStr = await (async () => {
+    if (e.queryStringParameters.coords) {
+      return e.queryStringParameters.coords;
+    } else {
+      try {
+        const res = await p({
+          url: `https://ipinfo.io/${ip}/geo`,
+          headers: {
+            "Authorization": `Bearer ${process.env.IPINFO_APIKEY}`
+          },
+          parse: "json",
+        });
+        return res.body.loc;
+      } catch (err) {
+        console.error("Failed to get IP address from ipinfo.io:", err);
+      }
+    }
+  })();
+  if (latLngStr.trim() === "") {
+    console.error(`Failed to get valid coordinates ('${latLngStr}')`);
+    return {
+      statusCode: 500,
+    };
   }
 
-  function gotLatLngStr() {
-    console.log({ latLngStr });
-    p({
+  try {
+    const res = await p({
       url: `https://api.darksky.net/forecast/${process.env.DARKSKY_APIKEY}/${latLngStr}?units=si&exclude=daily,hourly&lang=${lang}`,
-      parse: "json"
-    }).then(r => {
-      const response = {
-        weather: r.body,
-        debug: {
-          ip,
-          coords: latLngStr.split(",")
-        }
-      };
-      callback(null, {
-        status: 200,
-        body: JSON.stringify(response),
-      });
-    }).catch(err => {
-      console.error("Call to darksky.net API failed:", err);
-      callback(null, { status: 500 });
+      parse: "json",
     });
+    const response = {
+      weather: res.body,
+      debug: {
+        ip,
+        coords: latLngStr.split(","),
+      },
+    };
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(response),
+    };
+  } catch (err) {
+    console.error("Failed to get weather from darksky.net:", err);
+    return {
+      statusCode: 500,
+    };
   }
 };
