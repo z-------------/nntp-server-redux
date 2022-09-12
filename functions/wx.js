e281bc8eec602a6a958cd21f2e9dad68aec552f5
Zack Guard <zacharyguard@gmail.com>
2022-09-12 15:21:42 +0800

    Add debug logging

--- functions/wx.js
+++ functions/wx.js
@@ -1,8 +1,15 @@
 const p = require("phin");
+console.log("p is", p);
 
-exports.handler = (e, ctx, callback) => {  
-  let ip = e.queryStringParameters.ip || e.headers["x-nf-client-connection-ip"]; 
-  let lang = e.queryStringParameters.lang || "en";
+exports.handler = (e, ctx, callback) => {
+  console.log({ e });
+  console.log("e.queryStringParameters:", e.queryStringParameters);
+  console.log("e.headers:", e.headers);
+
+  const ip = e.queryStringParameters.ip || e.headers["x-nf-client-connection-ip"]; 
+  const lang = e.queryStringParameters.lang || "en";
+
+  console.log({ ip, lang });
 
   let latLngStr;
   if (e.queryStringParameters.coords) {
@@ -18,15 +25,19 @@ exports.handler = (e, ctx, callback) => {
     }).then(r => {
       latLngStr = r.body.loc;
       gotLatLngStr();
+    }).catch(err => {
+      console.error("Call to ipinfo.io API failed:", err);
+      callback(null, { status: 500 });
     });
   }
 
   function gotLatLngStr() {
+    console.log({ latLngStr });
     p({
       url: `https://api.darksky.net/forecast/${process.env.DARKSKY_APIKEY}/${latLngStr}?units=si&exclude=daily,hourly&lang=${lang}`,
       parse: "json"
     }).then(r => {
-      let response = {
+      const response = {
         weather: r.body,
         debug: {
           ip,
@@ -37,6 +48,9 @@ exports.handler = (e, ctx, callback) => {
         status: 200,
         body: JSON.stringify(response),
       });
+    }).catch(err => {
+      console.error("Call to darksky.net API failed:", err);
+      callback(null, { status: 500 });
     });
   }
 };
